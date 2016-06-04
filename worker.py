75a1200ac98dfa6d9323d1c63ff8cab6e462e48d
Andy Halterman <ahalterman0@gmail.com>
2016-06-04 15:38:50 -0400

    Rename

--- worker.py
+++ worker.py
@@ -1,6 +1,30 @@
 import pika
 import time
+import json
+import glob
+import csv
+import datetime
+from multiprocessing import Pool
+from pymongo import MongoClient
+import logging
+import sys
+import utilities
 
+# Read config file
+ln_user, ln_password, db_collection, whitelist_file, pool_size, log_dir, log_level, auth_db, auth_user, auth_pass, db_host = utilities.parse_config()
+
+# Set up connection to Mongo
+if db_host:
+    connection = MongoClient(host=db_host)
+else:
+    connection = MongoClient()
+
+db = connection.lexisnexis
+collection = db[db_collection]
+
+
+# Set up connection to queue.
+# The queue contains source-days left to download.
 credentials = pika.PlainCredentials('guest', 'guest')
 parameters = pika.ConnectionParameters('localhost', 5672, '/', credentials)
 connection = pika.BlockingConnection(parameters)
@@ -8,11 +32,14 @@ channel = connection.channel()
 channel.queue_declare(queue='cloacina_process2', durable=True)
 channel.confirm_delivery()
 
+
+
+# Handle the rate limit
 global doc_count
 doc_count = 0
 print doc_count
 
-def callback(ch, method, properties, body):
+def download(ch, method, properties, body):
     print(" [x] Received %r" % body)
     global doc_count
     doc_count += 1
@@ -24,7 +51,7 @@ def callback(ch, method, properties, body):
         ch.basic_cancel(consumer_tag = '', nowait = True)
 
 channel.basic_qos(prefetch_count=1)
-channel.basic_consume(callback,
+channel.basic_consume(download,
                               queue='cloacina_process2')
 
 channel.start_consuming()
