fd137dac602959707eaea1a154fb8027bb9e00cb
Andy Halterman <ahalterman0@gmail.com>
2016-06-04 15:27:08 -0400

    Multiple workers, exit on command

--- worker_unlimited.py
+++ worker_unlimited.py
@@ -8,11 +8,20 @@ channel = connection.channel()
 channel.queue_declare(queue='cloacina_process2', durable=True)
 channel.confirm_delivery()
 
+global doc_count
+doc_count = 0
+print doc_count
 
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
+    global doc_count
+    doc_count += 1
+    print doc_count
     time.sleep(1)
     ch.basic_ack(delivery_tag = method.delivery_tag)
+    if doc_count > 11:
+        # use the incorrect nowait arg to kill it. Otherwise it marches on.
+        ch.basic_cancel(consumer_tag = '', nowait = True)
 
 channel.basic_qos(prefetch_count=1)
 channel.basic_consume(callback,
